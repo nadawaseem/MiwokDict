ee7edc74a557216a05784cbcc6fbadb932dc3f47
Nada <little.nado12@gmail.com>
2019-06-25 04:24:13 +0200

    constructing XML files

---
+++ app/src/main/java/com/example/android/miwokdict/Animals.java
@@ -0,0 +1,4 @@
+package com.example.android.miwokdict;
+
+public class Animals {
+}
---
+++ app/src/main/java/com/example/android/miwokdict/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.miwokdict;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+    }
+    public void openNumbersActivity(View v){
+        Intent intent = new Intent(this, Numbers.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        startActivity(intent);
+    }
+}
