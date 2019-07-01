5ffde8f21e2e410bfcf6acbd4c3a264a90dfb319
Nada <little.nado12@gmail.com>
2019-07-01 02:42:00 +0200

    implementing onClick method to open the fields of the dictionary by Intents

--- app/src/main/java/com/example/android/miwokdict/Animals.java
+++ app/src/main/java/com/example/android/miwokdict/Animals.java
@@ -1,4 +1,13 @@
 package com.example.android.miwokdict;
 
-public class Animals {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Animals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animals);
+    }
 }
--- app/src/main/java/com/example/android/miwokdict/MainActivity.java
+++ app/src/main/java/com/example/android/miwokdict/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+
+
+        TextView numbers = findViewById(R.id.numbers);
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Numbers.class);
+                startActivity(i);
+            }
+        });
+
+
+        TextView animals = findViewById(R.id.animals);
+
+        animals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Animals.class);
+                startActivity(i);
+            }
+        });
+
+
+        TextView colors = findViewById(R.id.colors);
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Colors.class);
+                startActivity(i);
+            }
+        });
+
+
+        TextView familyMembers = findViewById(R.id.family);
+
+        familyMembers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FamilyMembers.class);
+                startActivity(i);
+            }
+        });
+
+
+        TextView phrases = findViewById(R.id.phrases);
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Phrases.class);
+                startActivity(i);
+            }
+        });
+
+
     }
-    public void openNumbersActivity(View v){
-        Intent intent = new Intent(this, Numbers.class);
-        intent.setAction(Intent.ACTION_VIEW);
-        startActivity(intent);
-    }
+
+
+
+
+
 }
