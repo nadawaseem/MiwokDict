74e793e6ca026bb716a64d558caf91bfe0e0bee5
Nada <little.nado12@gmail.com>
2019-07-05 05:43:53 +0200

    testing gridView with the generics class ArrayAdapter

--- app/src/main/java/com/example/android/miwokdict/Numbers.java
+++ app/src/main/java/com/example/android/miwokdict/Numbers.java
@@ -1,7 +1,11 @@
 package com.example.android.miwokdict;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class Numbers extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class Numbers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> englishWords = new ArrayList<>();
+        englishWords.add(0,"one");
+        englishWords.add(1,"two");
+        englishWords.add(2,"three");
+        englishWords.add(3,"four");
+        englishWords.add(4,"five");
+        englishWords.add(5,"six");
+        englishWords.add(6,"seven");
+        englishWords.add(7,"eight");
+        englishWords.add(8,"nine");
+        englishWords.add(9,"ten");
+
+
+
+
+
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, englishWords);
+
+        GridView GridView =  findViewById(R.id.list);
+
+        GridView.setAdapter(itemsAdapter);
+
+
+
+
+
+
     }
 }
